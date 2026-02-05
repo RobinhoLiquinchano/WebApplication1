19a2346e92583888bb1eca3acc634b3443576421
Robinho Liquinchano <robinholiquinchano@gmail.com>
2026-02-05 08:38:54 -0500

    aaaa

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,28 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Sacamos Swagger del IF para que Render lo pueda mostrar
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Votos v1");
+    c.RoutePrefix = string.Empty; // Para que cargue al abrir el link principal
+});
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
-
+// app.UseHttpsRedirection(); // Mejor dejarlo comentado en Render
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
